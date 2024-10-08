ff498f373b8f34fc62dff279b6db1bfe239a47b6
Pream-Kumar <pream2417@gmail.com>
2024-10-08 18:31:09 +0530

    commit @8th oct

--- src/test/java/com/movie/BookMyCineApplicationTests.java
+++ src/test/java/com/movie/BookMyCineApplicationTests.java
@@ -1,13 +1,12 @@
 package com.movie;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookMyCineApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
