2473a9dc65162b5854b0b7a1624fbb0a02b24939
Pream-Kumar <pream2417@gmail.com>
2024-10-09 18:39:52 +0530

    handling swagger api

--- src/main/java/com/movie/BookMyCineApplication.java
+++ src/main/java/com/movie/BookMyCineApplication.java
@@ -3,7 +3,10 @@ package com.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
 @SpringBootApplication
+@EnableOpenApi
 public class BookMyCineApplication {
 
 	public static void main(String[] args) {
