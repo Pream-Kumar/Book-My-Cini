f33fa426e1cb7a1057cba232e48a57dc77c89e81
Pream-Kumar <pream2417@gmail.com>
2024-10-26 10:40:04 +0530

    initial commit

---
+++ src/main/java/com/movie/service/SeatService.java
@@ -0,0 +1,38 @@
+package com.movie.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.movie.model.Seat;
+import com.movie.repository.SeatRepository;
+
+@Service
+public class SeatService {
+
+	@Autowired
+	private SeatRepository seatRepo;
+
+	public List<Seat> getAllSeats() {
+		return seatRepo.findAll();
+	}
+
+	public List<Seat> getSeatsByScreen(Long screenId) {
+		return seatRepo.findByScreenId(screenId);
+	}
+
+	public Optional<Seat> getSeatById(Long seatId) {
+		return seatRepo.findById(seatId);
+	}
+
+	public Seat addSeat(Seat seat) {
+		return seatRepo.save(seat);
+	}
+
+	public void deleteSeat(Long seatId) {
+		seatRepo.deleteById(seatId);
+	}
+
+}
