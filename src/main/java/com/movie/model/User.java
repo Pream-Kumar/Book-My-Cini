fce84bef647375677ab8de51afd61d04fc8e2918
Pream-Kumar <pream2417@gmail.com>
2025-03-21 21:37:01 +0530

    Will work with security
    Just static user name and password added

--- src/main/java/com/movie/model/User.java
+++ src/main/java/com/movie/model/User.java
@@ -2,17 +2,13 @@ package com.movie.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Data
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
@@ -24,42 +20,7 @@ public class User implements UserDetails {
     private String password;
     private LocalDateTime createdAt;
 
-    @ManyToOne
-    @JoinColumn(name = "role_id")
-    private Role role;
 
     @OneToMany(mappedBy = "user")
     private List<Booking> bookings;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    // Implementing UserDetails methods
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(role);  // Role should implement GrantedAuthority
-    }
-    @Override
-    public String getUsername() {
-        return email;  // Using email as username
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
